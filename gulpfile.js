416533b6e257c3eac02213a35d60b00abdf64b01
Juan Alvarez <juanandres140299@gmail.com>
2021-06-05 16:34:38 -0400

    Gulp Config

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,35 @@
 const gulp = require('gulp')
+const {series} = require('gulp')
 const sass = require('gulp-sass')
 const uglifycss = require('gulp-uglifycss')
+const uglify = require('gulp-uglify')
+const pipeline = require('readable-stream').pipeline
 
 sass.compiler = require('node-sass')
 
-gulp.task('sass', () => {
-  return gulp.src('./sass/*.scss')
+const compileSass = cb => {
+  gulp.src('./sass/*.scss')
   .pipe(sass().on('error', sass.logError))
   .pipe(gulp.dest('./public/styles'))
-})
+  cb()
+}
 
-gulp.task('css', () => {
-  return gulp.src('./public/styles/*.css')
+const uglifyCss = cb => {
+  gulp.src('./public/styles/*.css')
   .pipe(uglifycss({
     "uglyComments": true
   }))
   .pipe(gulp.dest('./public/dist'))
-})
+  cb()
+}
+
+const compileJs = cb => {
+  pipeline(
+    gulp.src('./public/scripts/*.js'),
+    uglify(),
+    gulp.dest('./public/dist')    
+  )
+  cb()
+}
+
+exports.build = series(compileSass, uglifyCss, compileJs)
---
+++ public/scripts/navbar.js
@@ -0,0 +1,23 @@
+const blackBackground = document.getElementsByClassName("black-background")[0]
+const openButton = document.getElementsByClassName("open_menu")[0]
+const closeButton = document.getElementsByClassName("close_menu")[0]
+const navbarContainer = document.getElementsByClassName(
+  "navbar_items_container"
+)[0]
+
+const openMenu = () => {
+  openButton.style.display = "none"
+  body.style.overflowY = "hidden"
+  blackBackground.classList.add("background-change")
+  blackBackground.style.display = "block"
+  navbarContainer.classList.add("slide")
+}
+
+const closeMenu = () => {
+  openButton.style.display = "block"
+  body.style.overflowY = "auto"
+  blackBackground.classList.remove("background-change")
+
+  setTimeout(() => (blackBackground.style.display = "none"), 300)
+  navbarContainer.classList.remove("slide")
+}
