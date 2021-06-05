5740dc640447bab623f4351f21e2e960a9b604c2
Juan Alvarez <juanandres140299@gmail.com>
2021-06-05 17:28:18 -0400

    Folder structure. Gulp tasks for watch changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp')
-const {series} = require('gulp')
+const {series, watch, parallel} = require('gulp')
 const sass = require('gulp-sass')
 const uglifycss = require('gulp-uglifycss')
 const uglify = require('gulp-uglify')
@@ -25,11 +25,22 @@ const uglifyCss = cb => {
 
 const compileJs = cb => {
   pipeline(
-    gulp.src('./public/scripts/*.js'),
+    gulp.src('./scripts/*.js'),
     uglify(),
     gulp.dest('./public/dist')    
   )
   cb()
 }
 
+const watchSass = cb => {
+  watch('./sass/*.scss', compileSass)
+  cb()
+}
+
+const watchJs = cb => {
+  watch('./scripts/*.js', compileJs)
+  cb()
+}
+
 exports.build = series(compileSass, uglifyCss, compileJs)
+exports.default = series(compileSass, uglifyCss, compileJs, parallel(watchSass, watchJs))
